0726d68d45545c618130f2c58434474962b80e15
nadir hemed <nadirhemed@gmail.com>
2021-08-09 17:54:42 +0300

    remove privilage infomation

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: 'mongodb+srv://admin-n:Kuntakinte@cluster0.fbqm9.mongodb.net/mernSocial'
+    mongoUri: 'mongodb+srv://admin-n:<password>@cluster0.fbqm9.mongodb.net/mernSocial'
 }
 export default config
 
