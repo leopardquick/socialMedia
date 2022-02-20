946886f705a3c97a651a1b5aaf125aa4aa4e921d
nadir hemed <nadirhemed@gmail.com>
2022-02-20 16:24:04 +0300

    Update config.js
    mongo

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: 'mongodb+srv://admin-n:<password>@cluster0.fbqm9.mongodb.net/mernSocial'
+    mongoUri: 'mongodb://localhost:27017/mernSocial'
 }
 export default config
 
